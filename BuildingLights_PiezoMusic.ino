6c9add2bdf9db364df6eca25d85d918b28d81ef9
Carmen Vargas <47091936+varsvisualizes@users.noreply.github.com>
2022-05-06 14:43:33 -0400

    Added in 2nd LED
    There are 2 LEDs in play for 2 buildings, so I added the 2nd LED into the code. I also modified the sensors to just 3 instead of 4, but I left in the 4th one commented out in case we decide to keep the 4th sensor after all.

--- BuildingLights_PiezoMusic.ino
+++ BuildingLights_PiezoMusic.ino
@@ -1,5 +1,5 @@
 /*
-   This code will:
+This code will:
 
    - show proposed building illumination colors
    - play music on the piezo buzzer based on a timer
@@ -27,10 +27,15 @@
 
 // initialize pins
 
-// RGB LED
-int redPin = 9;
-int greenPin = 10;
-int bluePin = 11;
+// RGB LED 1
+int redPin1 = 7;
+int greenPin1 = 8;
+int bluePin1 = 9;
+
+// RGB LED 2
+int redPin2 = 10;
+int greenPin2 = 11;
+int bluePin2 = 12;
 
 // piezo buzzer
 int speakerPin = 5;
@@ -50,11 +55,11 @@ int interval6 = (intervalLength * 30);
 int interval7 = (intervalLength * 35);
 int interval8 = (intervalLength * 43);
 
-// references values to remove offset
+// references values to remove offset for the flower sensors
 int ref0;
 int ref1;
 int ref2;
-int ref3;
+// int ref3;
 
 
 void setup() {
@@ -63,16 +68,19 @@ void setup() {
   startTime = millis();
 
   // establish the pins
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  pinMode(redPin1, OUTPUT);
+  pinMode(greenPin1, OUTPUT);
+  pinMode(bluePin1, OUTPUT);
+  pinMode(redPin2, OUTPUT);
+  pinMode(greenPin2, OUTPUT);
+  pinMode(bluePin2, OUTPUT);
   pinMode(speakerPin, OUTPUT);
 
   // reference values for the flowers
   ref0 = ADCTouch.read(A0, 500);
   ref1 = ADCTouch.read(A1, 500);
   ref2 = ADCTouch.read(A2, 500);
-  ref3 = ADCTouch.read(A4, 500);
+//  ref3 = ADCTouch.read(A4, 500);
 
   Serial.begin(9600);
 }
@@ -83,19 +91,19 @@ void loop() {
   int value0 = ADCTouch.read(A0);
   int value1 = ADCTouch.read(A1);
   int value2 = ADCTouch.read(A2);
-  int value3 = ADCTouch.read(A4);
+ // int value3 = ADCTouch.read(A4);
 
   // remove the offset
   value0 -= ref0;
   value1 -= ref1;
   value2 -= ref2;
-  value3 -= ref3;
+//  value3 -= ref3;
 
   // use  value of capacitor as a boolean
   int buttonState1 = (value0 > 40) ;
   int buttonState2 = (value1 > 40) ;
   int buttonState3 = (value2 > 40) ;
-  int buttonState4 = (value3 > 40) ;
+ // int buttonState4 = (value3 > 40) ;
 
   // get the current time
   currentTime = millis();
@@ -117,20 +125,24 @@ void loop() {
     Serial.println("pressed");
 
   }
-  else if (buttonState4 == 1) {
-    //flower #4
-    tone(speakerPin, NOTE_B6);
-    Serial.println("pressed");
-  }
-  // if no buttons are pressed, launch the building display & music timer
-  else if (buttonState1 != 1 && buttonState2 != 1 && buttonState3 != 1 && buttonState4 != 1) {
+//  else if (buttonState4 == 1) {
+//    //flower #4
+//    tone(speakerPin, NOTE_B6);
+//    Serial.println("pressed");
+//  }
+  // if no flowers are pressed, launch the building display & music timer
+  else if (buttonState1 != 1 && buttonState2 != 1 && buttonState3 != 1) {
     if (currentTime - startTime >= interval1 && currentTime - startTime < interval2) {
 
       Serial.println("Playing interval 1");
 
-      digitalWrite(redPin, LOW);
-      digitalWrite(greenPin, HIGH);
-      digitalWrite(bluePin, HIGH);
+      digitalWrite(redPin1, LOW);
+      digitalWrite(greenPin1, HIGH);
+      digitalWrite(bluePin1, HIGH);
+
+      digitalWrite(redPin2, LOW);
+      digitalWrite(greenPin2, HIGH);
+      digitalWrite(bluePin2, HIGH);
 
       tone(speakerPin, 1175, (1000 / 4));
       delay((1000 / 4) * 1.30);
@@ -143,9 +155,13 @@ void loop() {
 
       Serial.println("Playing interval 2");
 
-      digitalWrite(redPin, HIGH);
-      digitalWrite(greenPin, LOW);
-      digitalWrite(bluePin, HIGH);
+      digitalWrite(redPin1, HIGH);
+      digitalWrite(greenPin1, LOW);
+      digitalWrite(bluePin1, HIGH);
+
+      digitalWrite(redPin2, HIGH);
+      digitalWrite(greenPin2, LOW);
+      digitalWrite(bluePin2, HIGH);
 
       tone(speakerPin, 1480, (1000 / 4));
       delay((1000 / 4) * 1.30);
@@ -159,9 +175,13 @@ void loop() {
 
       Serial.println("Playing interval 3");
 
-      digitalWrite(redPin, HIGH);
-      digitalWrite(greenPin, HIGH);
-      digitalWrite(bluePin, LOW);
+      digitalWrite(redPin1, HIGH);
+      digitalWrite(greenPin1, HIGH);
+      digitalWrite(bluePin1, LOW);
+
+      digitalWrite(redPin2, HIGH);
+      digitalWrite(greenPin2, HIGH);
+      digitalWrite(bluePin2, LOW);
 
       tone(speakerPin, 1175, (1000 / 4));
       delay((1000 / 4) * 1.30);
@@ -174,9 +194,13 @@ void loop() {
 
       Serial.println("Playing interval 4");
 
-      digitalWrite(redPin, LOW);
-      digitalWrite(greenPin, LOW);
-      digitalWrite(bluePin, HIGH);
+      digitalWrite(redPin1, LOW);
+      digitalWrite(greenPin1, LOW);
+      digitalWrite(bluePin1, HIGH);
+      
+      digitalWrite(redPin2, LOW);
+      digitalWrite(greenPin2, LOW);
+      digitalWrite(bluePin2, HIGH);
 
       tone(speakerPin, 1976, (1000 / 4));
       delay((1000 / 4) * 1.30);
@@ -189,9 +213,13 @@ void loop() {
 
       Serial.println("Playing interval 5");
 
-      digitalWrite(redPin, LOW);
-      digitalWrite(greenPin, HIGH);
-      digitalWrite(bluePin, LOW);
+      digitalWrite(redPin1, LOW);
+      digitalWrite(greenPin1, HIGH);
+      digitalWrite(bluePin1, LOW);
+
+      digitalWrite(redPin2, LOW);
+      digitalWrite(greenPin2, HIGH);
+      digitalWrite(bluePin2, LOW);
 
       tone(speakerPin, 2349, (1000 / 4));
       delay((1000 / 4) * 1.30);
@@ -204,9 +232,14 @@ void loop() {
 
       Serial.println("Playing interval 6");
 
-      digitalWrite(redPin, HIGH);
-      digitalWrite(greenPin, LOW);
-      digitalWrite(bluePin, LOW);
+      digitalWrite(redPin1, HIGH);
+      digitalWrite(greenPin1, LOW);
+      digitalWrite(bluePin1, LOW);
+
+      digitalWrite(redPin2, HIGH);
+      digitalWrite(greenPin2, LOW);
+      digitalWrite(bluePin2, LOW);
+
 
       tone(speakerPin, 1175, (1000 / 4));
       delay((1000 / 4) * 1.30);
@@ -219,9 +252,13 @@ void loop() {
 
       Serial.println("Playing interval 7");
 
-      digitalWrite(redPin, LOW);
-      digitalWrite(greenPin, LOW);
-      digitalWrite(bluePin, LOW);
+      digitalWrite(redPin1, LOW);
+      digitalWrite(greenPin1, LOW);
+      digitalWrite(bluePin1, LOW);
+
+      digitalWrite(redPin2, LOW);
+      digitalWrite(greenPin2, LOW);
+      digitalWrite(bluePin2, LOW);
 
       tone(speakerPin, 1175, (1000 / 4));
       delay((1000 / 4) * 1.30);
