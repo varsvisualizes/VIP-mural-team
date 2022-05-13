5a2c73bf97e4e7a4c471d75c79b605830640966a
Carmen Vargas <47091936+varsvisualizes@users.noreply.github.com>
2022-05-13 14:28:31 -0400

    Added building light patterns for flower switches

--- BuildingLights_PiezoMusic.ino
+++ BuildingLights_PiezoMusic.ino
@@ -1,11 +1,11 @@
 /*
-This code will:
+  This code will:
 
    - show proposed building illumination colors
    - play music on the piezo buzzer based on a timer
    - play a particular note when a flower is pressed
 
-   When the flowers are not interacted with, the buildings will play music patterns and display colors based on a timer. 
+   When the flowers are not interacted with, the buildings will play music patterns and display colors based on a timer.
    If the flowers are being touched, this will override the building timer and just play the note corresponding to the flower.
 
    These are the notes used for the music when the buildings are running on a timer:
@@ -80,7 +80,7 @@ void setup() {
   ref0 = ADCTouch.read(A0, 500);
   ref1 = ADCTouch.read(A1, 500);
   ref2 = ADCTouch.read(A2, 500);
-//  ref3 = ADCTouch.read(A4, 500);
+  //  ref3 = ADCTouch.read(A4, 500);
 
   Serial.begin(9600);
 }
@@ -91,45 +91,114 @@ void loop() {
   int value0 = ADCTouch.read(A0);
   int value1 = ADCTouch.read(A1);
   int value2 = ADCTouch.read(A2);
- // int value3 = ADCTouch.read(A4);
+  // int value3 = ADCTouch.read(A4);
 
   // remove the offset
   value0 -= ref0;
   value1 -= ref1;
   value2 -= ref2;
-//  value3 -= ref3;
+  //  value3 -= ref3;
 
   // use  value of capacitor as a boolean
   int buttonState1 = (value0 > 40) ;
   int buttonState2 = (value1 > 40) ;
   int buttonState3 = (value2 > 40) ;
- // int buttonState4 = (value3 > 40) ;
+  // int buttonState4 = (value3 > 40) ;
 
   // get the current time
   currentTime = millis();
 
   if (buttonState1 == 1) {
+
     //flower #1
-    Serial.println("pressed");
+    Serial.println("pressed 1");
     //iterate over notes of melody
     tone(speakerPin, NOTE_E6);
+
+    // purple pulse pattern - LED 1
+    for (int j = 0; j < 3; j++) {
+      for (int i = 0; i < 100; i += 10) {
+        analogWrite(redPin1, i);
+        analogWrite(bluePin1, i);
+        digitalWrite(greenPin1, HIGH);
+        delay(20);
+      }
+      for (int i = 100; i > 0; i -= 10) {
+        analogWrite(redPin1, i);
+        analogWrite(bluePin1, i);
+        digitalWrite(greenPin1, HIGH);
+        delay(20);
+      }
+    }
+
+    // turn off LED 2
+    digitalWrite(redPin2, HIGH);
+    digitalWrite(greenPin2, HIGH);
+    digitalWrite(bluePin2, HIGH);
+
   }
   else if (buttonState2 == 1) {
     //flower #2
     tone(speakerPin, NOTE_G5);
-    Serial.println("pressed");
+    Serial.println("pressed 2");
+
+
+    // cyan / green alternating pattern - LED 2
+    for (int j = 0; j < 3; j++) {
+      for (int i = 0; i < 255; i += 10) {
+        analogWrite(greenPin2, (i * 0.5));
+        analogWrite(bluePin2, i);
+        digitalWrite(redPin2, HIGH);
+        delay(20);
+      }
+      for (int i = 255; i > 0; i -= 10) {
+        analogWrite(bluePin2, i);
+        digitalWrite(greenPin2, LOW);
+        digitalWrite(redPin2, HIGH);
+        delay(20);
+      }
+
+    // turn off LED 1
+    digitalWrite(redPin1, HIGH);
+    digitalWrite(greenPin1, HIGH);
+    digitalWrite(bluePin1, HIGH);
+
+    }
+
   }
   else if (buttonState3 == 1) {
     //flower #3
     tone(speakerPin, NOTE_D7);
-    Serial.println("pressed");
+    Serial.println("pressed 3");
+
+    // bouncing white light between buildings
+
+    digitalWrite(redPin1, HIGH);
+    digitalWrite(greenPin1, HIGH);
+    digitalWrite(bluePin1, HIGH);
+
+    digitalWrite(redPin2, LOW);
+    digitalWrite(greenPin2, LOW);
+    digitalWrite(bluePin2, LOW);
+
+    delay(150);
+
+    digitalWrite(redPin2, HIGH);
+    digitalWrite(greenPin2, HIGH);
+    digitalWrite(bluePin2, HIGH);
+
+    digitalWrite(redPin1, LOW);
+    digitalWrite(greenPin1, LOW);
+    digitalWrite(bluePin1, LOW);
+
+    delay(150);
 
   }
-//  else if (buttonState4 == 1) {
-//    //flower #4
-//    tone(speakerPin, NOTE_B6);
-//    Serial.println("pressed");
-//  }
+  //  else if (buttonState4 == 1) {
+  //    //flower #4
+  //    tone(speakerPin, NOTE_B6);
+  //    Serial.println("pressed");
+  //  }
   // if no flowers are pressed, launch the building display & music timer
   else if (buttonState1 != 1 && buttonState2 != 1 && buttonState3 != 1) {
     if (currentTime - startTime >= interval1 && currentTime - startTime < interval2) {
@@ -197,7 +266,7 @@ void loop() {
       digitalWrite(redPin1, LOW);
       digitalWrite(greenPin1, LOW);
       digitalWrite(bluePin1, HIGH);
-      
+
       digitalWrite(redPin2, LOW);
       digitalWrite(greenPin2, LOW);
       digitalWrite(bluePin2, HIGH);
